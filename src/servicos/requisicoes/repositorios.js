12a1656e3b8c2bdffcde72af6ab0ffb5e3d612b6
Vinicius Canaan <60820733+ViniciusCanaan@users.noreply.github.com>
2022-03-28 22:01:58 -0300

    Criando repositorio do usuário através do método post

--- src/servicos/requisicoes/repositorios.js
+++ src/servicos/requisicoes/repositorios.js
@@ -10,3 +10,34 @@ export async function pegarRepositoriosDoUsuario(id){
         return []
     }
 }
+
+export async function salvarRepositoriosDoUsuario(postId, nome, data, id){
+    try{
+        await api.put(`/repos/${id}`,{
+            name: nome,
+            data: data,
+            postId: postId,
+            id: id
+        });
+        return 'sucesso'
+    }
+    catch(error){
+        console.log(error)
+        return 'erro'
+    }
+}
+
+export async function criarRepositoriosDoUsuario(postId, nome, data){
+    try{
+        await api.post(`/repos/`,{
+            name: nome,
+            data: data,
+            postId: postId,
+        });
+        return 'sucesso'
+    }
+    catch(error){
+        console.log(error)
+        return 'erro'
+    }
+}
